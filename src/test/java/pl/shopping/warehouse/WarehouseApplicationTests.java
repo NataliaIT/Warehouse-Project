c3bdb78d51343a9e6e8ca417eb288fa983c66360
Natalia <natikrak@gmail.com>
2020-03-10 22:56:09 +0100

    Initial commit

---
+++ src/test/java/pl/shopping/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.shopping.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
